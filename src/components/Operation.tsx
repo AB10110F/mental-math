5b4c2a837f90f65234dde1c6df297899501b34d8
A. Bryant <W.Sword.09@hotmail.com>
2026-05-09 18:10:09 -0600

    add logic to operation

--- src/components/Operation.tsx
+++ src/components/Operation.tsx
@@ -1,7 +1,10 @@
+import { useRef } from "react";
+
 type OperationProps = {
+  onCorrect: (time: number, result: number) => void;
   digits1: number,
   digits2: number,
-  sign: "+" | "-" | "*" | "/"
+  sign: "+" | "-" | "×" | "÷"
 }
 
 function randomize(digits: number) {
@@ -10,25 +13,58 @@ function randomize(digits: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function getResult(a: number, b: number, sign: string) {
+  switch (sign) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '×': return a * b;
+    case '÷': return a / b;
+  }
+}
+
 export default function Operation({
+  onCorrect,
   digits1,
   digits2,
   sign }: OperationProps) {
 
-  const number1 = randomize(digits1)
-  const number2 = randomize(digits2)
+  const a = randomize(digits1)
+  const b = randomize(digits2)
+  const startTime = Date.now();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const result = getResult(a, b, sign);
+
+  const checkResult = () => {
+    const value = inputRef.current?.value || "";
+    const endTime = Date.now();
+    const time = (endTime - startTime) / 1000;
+
+    if (parseInt(value) === result) {
+      onCorrect(time, result);
+    }
+  };
+
+  const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
+    const input = e.currentTarget;
+    input.value = input.value.replace(/\D/g, '');
+  };
 
   return (
     <section className="flex flex-col w-fit text-7xl text-right">
-      <span>{number1}</span>
+      <span>{a}</span>
       <section className="flex justify-between">
         <span>{sign}</span>
-        <span>{number2}</span>
+        <span>{b}</span>
       </section>
       <span className="my-2 h-1 border-0 bg-gray-300"></span>
       <input
-        type="number"
-        // pattern="\d*"
+        type="text"
+        inputMode="numeric"
+        autoFocus
+        maxLength={result?.toString().length}
+        ref={inputRef}
+        onInput={handleInput}
+        onChange={checkResult}
         className="text-right w-0 min-w-full border-none outline-none caret-transparent [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" />
     </section>
   );
