1c87493cdd6058bd0c43dbd2ec5ff67a720e7dbc
A. Bryant <W.Sword.09@hotmail.com>
2026-05-09 18:09:51 -0600

    add logic to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,12 @@
 type ButtonProps = {
   color: "bg-blue-500 hover:bg-blue-700" | "bg-red-500 hover:bg-red-700",
   text: string
+  onClick: () => void;
 }
-export default function Button({ color, text }: ButtonProps) {
+export default function Button({ color, text, onClick }: ButtonProps) {
 
   return (
-    <button className={`${color} text-white font-bold py-2 px-4 rounded-lg transition-colors cursor-pointer`}>
+    <button onClick={onClick} className={`${color} text-white font-bold py-2 px-4 rounded-lg transition-colors cursor-pointer`}>
       {text}
     </button>
   );
